210af768de7bb04c60c98ab28666b17fce206383
Xanders <1496973+xanderssamoth@users.noreply.github.com>
2024-03-20 16:33:05 +0100

    Update forgot-password.blade.php

--- resources/views/auth/forgot-password.blade.php
+++ resources/views/auth/forgot-password.blade.php
@@ -5,7 +5,7 @@
                                 <div class="card border rounded-0 text-center shadow-0">
                                     <div class="card-body py-5">
                                         <h4 class="h4 mb-2 text-center">@lang('auth.reset-password')</h4>
-                                        <p class="lead mb-4 text-secondary">@lang('auth.verify-email-phone')</p>
+                                        <p class="mb-4 text-secondary">@lang('auth.verify-email-phone')</p>
 
                                         <form method="POST" action="{{ route('register') }}">
     @csrf
@@ -13,41 +13,37 @@
                                             <input type="hidden" name="redirect" value="reset_password">
 
                                             <div class="row mb-4">
-                                                <div class="col-12">
-                                                    <div class="form-floating">
-                                                        <input type="text" name="register_email" id="register_email" class="form-control" placeholder="@lang('miscellaneous.email')" value="{{ \Session::has('error_message') ? explode('-', explode('~', \Session::get('error_message'))[1])[2] : '' }}">
-                                                        <label for="register_email">@lang('miscellaneous.email')</label>
-                                                    </div>
+                                                <div class="form-floating mb-3">
+                                                    <input type="text" name="register_email" id="register_email" class="form-control" placeholder="@lang('miscellaneous.email')" value="{{ \Session::has('error_message') ? explode('-', explode('~', \Session::get('error_message'))[1])[2] : '' }}">
+                                                    <label for="register_email">@lang('miscellaneous.email')</label>
                                                 </div>
 
-                                                <div class="col-12">
-                                                    <div class="row g-3">
-                                                        <div class="col-sm-5">
-                                                            <div class="form-floating pt-0">
-                                                                <select name="select_country" id="select_country1" class="form-select pt-2 shadow-0">
-                                                                    <option class="small" selected disabled>@lang('miscellaneous.choose_country')</option>
+                                                <div class="row g-3">
+                                                    <div class="col-sm-5">
+                                                        <div class="form-floating pt-0">
+                                                            <select name="select_country" id="select_country1" class="form-select pt-2 shadow-0">
+                                                                <option class="small" selected disabled>@lang('miscellaneous.choose_country')</option>
     @forelse ($countries as $country)
-                                                                    <option value="{{ '+' . $country->country_phone_code . '-' . $country->id }}">{{ $country->country_name }}</option>
+                                                                <option value="{{ '+' . $country->country_phone_code . '-' . $country->id }}">{{ $country->country_name }}</option>
     @empty
-                                                                    <option>@lang('miscellaneous.empty_list')</option>
+                                                                <option>@lang('miscellaneous.empty_list')</option>
     @endforelse
-                                                                </select>
-                                                            </div>
+                                                            </select>
                                                         </div>
+                                                    </div>
 
-                                                        <div class="col-sm-7">
-                                                            <div class="input-group">
-                                                                <span id="phone_code_text1" class="input-group-text d-inline-block h-100" style="padding-top: 0.3rem; padding-bottom: 0.5rem; line-height: 1.35;">
-                                                                    <small class="text-muted m-0 p-0" style="font-size: 0.75rem;">@lang('miscellaneous.phone_code')</small><br>
-                                                                    <span class="text-value">xxxx</span> 
-                                                                </span>
+                                                    <div class="col-sm-7">
+                                                        <div class="input-group">
+                                                            <span id="phone_code_text1" class="input-group-text d-inline-block h-100" style="padding-top: 0.3rem; padding-bottom: 0.5rem; line-height: 1.35;">
+                                                                <small class="text-muted m-0 p-0" style="font-size: 0.75rem;">@lang('miscellaneous.phone_code')</small><br>
+                                                                <span class="text-value">xxxx</span> 
+                                                            </span>
 
-                                                                <div class="form-floating">
-                                                                    <input type="hidden" id="country_id1" name="country_id" value="">
-                                                                    <input type="hidden" id="phone_code1" name="phone_code" value="">
-                                                                    <input type="tel" name="phone_number" id="phone_number" class="form-control" placeholder="@lang('miscellaneous.phone')">
-                                                                    <label for="phone_number">@lang('miscellaneous.phone')</label>
-                                                                </div>
+                                                            <div class="form-floating">
+                                                                <input type="hidden" id="country_id1" name="country_id" value="">
+                                                                <input type="hidden" id="phone_code1" name="phone_code" value="">
+                                                                <input type="tel" name="phone_number" id="phone_number" class="form-control" placeholder="@lang('miscellaneous.phone')">
+                                                                <label for="phone_number">@lang('miscellaneous.phone')</label>
                                                             </div>
                                                         </div>
                                                     </div>
