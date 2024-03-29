87d0bd86cbb1fd67790dac0e6f146d94808ae492
Xanders <1496973+xanderssamoth@users.noreply.github.com>
2024-03-29 01:51:59 +0100

    Update RegisteredUserController.php

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers\Auth;
 
 use Illuminate\Http\Request;
 use Illuminate\Http\RedirectResponse;
-use Illuminate\Support\Facades\Auth;
 use Illuminate\View\View;
 use App\Http\Controllers\Controller;
 use App\Http\Controllers\ApiClientManager;
