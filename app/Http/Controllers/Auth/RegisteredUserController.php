a6645b23e33da18e445d3211aeccd2f604365b5a
Xanders <1496973+xanderssamoth@users.noreply.github.com>
2024-04-02 07:15:38 +0100

    Update RegisteredUserController.php

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -170,7 +170,7 @@ class RegisteredUserController extends Controller
                         $apiURL = $request->check_param == 'email' ? getApiURL() . '/password_reset/search_by_email/' . $user_inputs['email'] : getApiURL() . '/password_reset/search_by_phone/' . $user_inputs['phone'];
                         $password_reset = $this::$api_client_manager::call('GET', $apiURL, null, $user_inputs);
 
-                        dd($password_reset);
+                        dd($apiURL);
 
                         if ($password_reset->success) {
                             return view('auth.register', [
