540d647762869e9c50bcfe38faac79d3c4625383
Xanders <1496973+xanderssamoth@users.noreply.github.com>
2024-05-10 13:18:38 +0100

    Update watchlist.blade.php

--- resources/views/partials/account/watchlist.blade.php
+++ resources/views/partials/account/watchlist.blade.php
@@ -6,9 +6,9 @@
                                     <div class="list-group list-group-flush">
                 @foreach ($watchlist as $item)
                                         <a href="{{ route('media.datas', ['id' => $item->media->id]) }}" class="list-group-item list-group-item-action">
-                                            <img src="{{ !empty($item->media->cover_url) ? $item->media->cover_url : asset('assets/img/blank-media-video.png') }}" alt="{{ $item->media->media_title }}" width="190" class="float-start rounded-4 me-3">
+                                            <img src="{{ !empty($item->media->cover_url) ? $item->media->cover_url : asset('assets/img/blank-media-video.png') }}" alt="{{ $item->media->media_title }}" width="160" class="float-start rounded-4 me-3">
                                             <h4 class="my-2 dktv-text-green fw-bold">{{ $item->media->media_title }}</h4>
-                                            <p class="text-muted">{{ !empty($item->media->media_description) ? Str::limit($item->media->media_description, 20, '...') :$item->media->author_names }}</p>
+                                            <p class="text-muted">{{ !empty($item->media->media_description) ? Str::limit($item->media->media_description, 20, '...') : $item->media->author_names }}</p>
                                         </a>
                 @endforeach
                                     </div>
