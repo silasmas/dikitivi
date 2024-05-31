d20daae7532490d84d927d29f627b4061bdfc6a2
xanderssamoth <1496973+xanderssamoth@users.noreply.github.com>
2024-05-31 06:20:12 +0100

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -385,14 +385,14 @@ if (request()->has('app_id')) {
                                     <!-- for-youth settings start -->
                                     <div title="@lang('miscellaneous.change')">
     @if ($for_youth == 1)
-                                        <a href="{{ route('choose_age', ['for_youth' => 0]) }}" class="btn dktv-btn-blue ms-3 rounded-pill shadow-0" style="padding: 0.3rem 0.6rem 0.4rem 0.6rem;">
+                                        <a href="{{ route('choose_age', ['for_youth' => 0]) }}" class="btn dktv-btn-blue ms-3 rounded-pill shadow-0" style="padding: 0.3rem 0.65rem 0.4rem 0.65rem;">
                                             <i class="bi bi-person-down me-sm-2 fs-5 align-middle"></i>
                                             <pan class="d-sm-inline-block d-none">@lang('miscellaneous.iam_adult')</pan>
                                         </a>
     @endif
 
     @if ($for_youth == 0)
-                                        <a href="{{ route('choose_age', ['for_youth' => 1]) }}" class="btn dktv-btn-yellow ms-3 rounded-pill shadow-0" style="padding: 0.3rem 0.6rem 0.4rem 0.6rem;">
+                                        <a href="{{ route('choose_age', ['for_youth' => 1]) }}" class="btn dktv-btn-yellow ms-3 rounded-pill shadow-0" style="padding: 0.3rem 0.65rem 0.4rem 0.65rem;">
                                             <i class="bi bi-person-up me-sm-2 fs-5 align-middle"></i>
                                             <pan class="d-sm-inline-block d-none">@lang('miscellaneous.iam_child')</pan>
                                         </a>
