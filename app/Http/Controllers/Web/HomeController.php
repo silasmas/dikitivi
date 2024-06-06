9d4317691040cd56cbe10f6bf50e4156efd97128
xanderssamoth <1496973+xanderssamoth@users.noreply.github.com>
2024-06-06 10:31:50 +0100

    Update HomeController.php

--- app/Http/Controllers/Web/HomeController.php
+++ app/Http/Controllers/Web/HomeController.php
@@ -1063,11 +1063,10 @@ class HomeController extends Controller
         $watchlist_type_name = 'Watchlist';
         $watchlist_type = $this::$api_client_manager::call('GET', getApiURL() . '/type/search/fr/' . $watchlist_type_name);
 
+        dd('TEST');
         if (session()->has('for_youth')) {
             if (session()->get('for_youth') == 1) {
                 if ($entity == 'preach') {
-                    dd($preach_category);
-
                     if (Auth::check()) {
                         // Select a user API
                         $user = $this::$api_client_manager::call('GET', getApiURL() . '/user/' . Auth::user()->id, Auth::user()->api_token);
