2e8bdd328013918da5fa19cb104ff4b17c4ccf86
xanderssamoth <1496973+xanderssamoth@users.noreply.github.com>
2024-06-12 10:11:46 +0100

    Update watchlist.blade.php

--- resources/views/partials/account/watchlist.blade.php
+++ resources/views/partials/account/watchlist.blade.php
@@ -6,14 +6,16 @@
 @if (count($watchlist) > 0)
                                     <div class="list-group list-group-flush">
     @foreach ($watchlist as $item)
+        @if (!empty($item->media))
                                         <a href="{{ route('media.datas', ['id' => $item->media->id]) }}" class="list-group-item list-group-item-action position-relative">
                                             <button type="button" class="btn btn-link text-muted py-1 rounded-pill float-end position-relative" style="z-index: 999; padding-left: 0.47rem; padding-right: 0.47rem;" title="@lang('miscellaneous.public.withdraw_watchlist')" data-bs-toggle="tooltip" data-bs-placement="left" data-watchlist-id="{{ $watchlist_id }}" onclick="event.preventDefault(); toggleAction(this, {{ $item->media->id }}, 'delete_from_watchlist');">
                                                 <i class="bi bi-x-lg"></i>
                                             </button>
                                             <img src="{{ !empty($item->media->cover_url) ? $item->media->cover_url : asset('assets/img/blank-media-video.png') }}" alt="{{ $item->media->media_title }}" width="160" class="float-sm-start rounded-4 me-3">
-                                            <h4 class="my-2 dktv-text-green fw-bold">{{ $item->media->media_title }}</h4>
+                                            <h4 class="my-2 dktv-text-green fw-bold">{{ $item->media->media_title ?? '' }}</h4>
                                             <p class="text-muted">{{ !empty($item->media->media_description) ? Str::limit($item->media->media_description, 20, '...') : $item->media->author_names }}</p>
                                         </a>
+        @endif
     @endforeach
                                     </div>
 
