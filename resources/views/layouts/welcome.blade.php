75a108d3dda8e51bf2812314a1e8bac8318356f3
xanderssamoth <1496973+xanderssamoth@users.noreply.github.com>
2024-07-09 16:39:39 +0100

    Update welcome.blade.php

--- resources/views/layouts/welcome.blade.php
+++ resources/views/layouts/welcome.blade.php
@@ -419,9 +419,7 @@
                     }
 
                     $.ajaxSetup({
-                        headers: {
-                            'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content');
-                        }
+                        headers: { 'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content') }
                     });
 
                     $.ajax({
