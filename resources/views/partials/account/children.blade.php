7dbc3abb196101728eb64ca8182669d84cbf1757
xsamtech <1496973+xanderssamoth@users.noreply.github.com>
2024-08-21 13:34:42 +0100

    Update children.blade.php

--- resources/views/partials/account/children.blade.php
+++ resources/views/partials/account/children.blade.php
@@ -89,10 +89,10 @@
         @if (count($viewed_medias) > 0)
                                         <div class="list-group">
             @foreach ($viewed_medias as $media)
-                                            <a href="{{ route('media.datas', ['id' => $media->id]) }}" class="list-group-item list-group-item-action position-relative">
-                                                <img src="{{ !empty($media->cover_url) ? $media->cover_url : asset('assets/img/blank-media-video.png') }}" alt="{{ $media->media_title }}" width="160" class="float-sm-start rounded-4 me-3">
-                                                <h4 class="my-2 dktv-text-green fw-bold">{{ $media->media_title }}</h4>
-                                                <p class="text-muted">{{ !empty($media->media_description) ? Str::limit($media->media_description, 20, '...') : $media->author_names }}</p>
+                                            <a href="{{ route('media.datas', ['id' => $media['id']]) }}" class="list-group-item list-group-item-action position-relative">
+                                                <img src="{{ !empty($media['cover_url']) ? $media['cover_url'] : asset('assets/img/blank-media-video.png') }}" alt="{{ $media['media_title'] }}" width="160" class="float-sm-start rounded-4 me-3">
+                                                <h4 class="my-2 dktv-text-green fw-bold">{{ $media['media_title'] }}</h4>
+                                                <p class="text-muted">{{ !empty($media['media_description']) ? Str::limit($media['media_description'], 20, '...') : $media['author_names'] }}</p>
                                             </a>
             @endforeach
                                         </div>
