54a44489b2d4b7dcc7f00f40334e5d2744e0a650
xsamtech <1496973+xanderssamoth@users.noreply.github.com>
2024-10-12 12:02:16 +0100

    n

--- resources/views/partials/about/about.blade.php
+++ resources/views/partials/about/about.blade.php
@@ -16,13 +16,13 @@
 
                 <div class="row">
                     <div class="col-sm-8">
-@foreach ($titles as $ttl)
-    @if ($ttl['ref'] == 'mission')
+@foreach ($subject['legal_info_titles'] as $ttl)
+    @if ($ttl['title_fr'] == 'Notre Mission')
                         <hr class="my-5">
 
                         <h3 class="h3 mb-4 fw-bold">{{ $ttl['title'] }}</h3>
 
-        @foreach ($ttl['contents'] as $cnt)
+        @foreach ($ttl['legal_info_contents'] as $cnt)
                         <div class="mb-4">
                             <p class="mb-1 fs-6 text-secondary"><i class="bi bi-chevron-double-right me-2 align-middle fs-5 text-danger"></i> {!! $cnt['content'] !!}</p>
                         </div>
@@ -32,7 +32,7 @@
     @else
                         <h3 class="h3 mb-4 fw-bold">{{ $ttl['title'] }}</h3>
 
-        @foreach ($ttl['contents'] as $cnt)
+        @foreach ($ttl['legal_info_contents'] as $cnt)
                         <div class="mb-4">
                             <h5 class="h5 mb-1 fw-semibold dktv-text-green">{{ $cnt['subtitle'] }}</h5>
 
