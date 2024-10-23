172f36024de0cc950aa4c21c6bfa0de95c017f4a
xsamtech <1496973+xanderssamoth@users.noreply.github.com>
2024-10-23 09:35:16 +0100

    Update datas.blade.php

--- resources/views/partials/media/datas.blade.php
+++ resources/views/partials/media/datas.blade.php
@@ -78,10 +78,8 @@ if (!empty($current_media['belongs_to'])) {
                                     </video>
                                 </div>
             @else
-                                <div class="ratio ratio-16x9">
-                                    <div class="plyr__video-embed" id="player">
-                                        <iframe src="{{ $current_media['media_url'] }}?rel=0" allowfullscreen allowtransparency></iframe>
-                                    </div>
+                                <div class="plyr__video-embed" id="player">
+                                    <iframe src="{{ $current_media['media_url'] }}?rel=0" allowfullscreen allowtransparency></iframe>
                                 </div>
             @endif
         @else
